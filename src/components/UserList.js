f665662d1d9194ddfcce2775b53e54925079d617
devjoemedia <joenart2@gmail.com>
2022-01-29 15:37:17 +0000

    prefix

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -4,28 +4,32 @@ import User from "./User";
 const UserList = ({ users, deleteUser, editUser }) => {
   return (
     <div>
-      <table className="table table-dark table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Gen</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => (
-            <User
-              key={index}
-              index={index}
-              user={user}
-              deleteUser={deleteUser}
-              editUser={editUser}
-            />
-          ))}
-        </tbody>
-      </table>
+      {users.length ? (
+        <table className="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Gen</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <User
+                key={index}
+                index={index}
+                user={user}
+                deleteUser={deleteUser}
+                editUser={editUser}
+              />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <h4 className="text-muted">No users available</h4>
+      )}
     </div>
   );
 };
