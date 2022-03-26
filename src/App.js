f8fbf295ca85917708db91e0151e734cc2b71826
devjoemedia <joenart2@gmail.com>
2022-03-26 14:43:44 +0000

    firebase crud

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setUsers } from "./actions";
 import Form from "./components/Form";
 import UserList from "./components/UserList";
+import firebase from "./firebase/config";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("users")
+      .onSnapshot((users) => {
+        let data = [];
+
+        users.forEach((user) => {
+          data.push(user.data());
+        });
+
+        dispatch(setUsers(data));
+      });
+  }, []);
+
   return (
     <div style={{ width: "600px", margin: "auto" }}>
       <h3 className="text-muted">UserList App</h3>
